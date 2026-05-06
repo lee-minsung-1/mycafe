5f57987f8ae6243cc55394bb4e3a23e64c6f903c
manager <manager@naver.com>
2026-05-06 14:20:02 +0900

    push by master 02

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main{
     static void main(String[] args) {
         System.out.println("1번째 실습_chulsoo.");
         System.out.println("3번째 실습_chulsoo.");
+        System.out.println("3번째 실습_chulsasdsadwwoo.");
     }
 }
