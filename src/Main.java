755a5a08f3b877b72b2477baef8767478e0dd7f1
minsung <2012dlalstjd@naver.com>
2026-05-06 14:21:22 +0900

    push by sport 01sadadsdaskkk

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main{
     static void main(String[] args) {
         System.out.println("1번째 실습_chulsoo.");
-        System.out.println("3번째 실습_chulsoo.");
+        System.out.println("2번째 실습_chuldsasoo.");
+        System.out.println("4번째 실습_chuldsasoo.");
     }
 }
